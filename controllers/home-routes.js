c414ba0447f65f5280c317cb45f1fa850faf5f39
Chris McLeod <mcleod32@live.com>
2022-03-08 19:30:17 -0600

    initial login commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,8 +6,9 @@ router.get('/', (req, res) => {
     res.render('homepage');
   });
 
-router.get('/login', (req, res) => {
-
+  router.get('/login', (req, res) => {
+  
+  
     res.render('login');
   });
 
