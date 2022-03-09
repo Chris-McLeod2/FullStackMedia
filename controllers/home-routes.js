d1647109a9724cccf6ec5a1b9fc13b689dabd848
Chris McLeod <mcleod32@live.com>
2022-03-08 18:48:39 -0600

    bare-bones handlebars pages loading

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,25 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-
 router.get('/', (req, res) => {
+    res.render('homepage');
+  });
 
-});
+router.get('/login', (req, res) => {
+
+    res.render('login');
+  });
+
+  router.get('/posts', (req, res) => {
+
+    res.render('posts');
+  });
+
+  router.get('/profile', (req, res) => {
+
+    res.render('profile');
+  });
+
 
 
 module.exports = router;
