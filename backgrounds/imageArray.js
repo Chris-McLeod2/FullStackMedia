5c49a93911aaaf34dc36f5c32c97d2144f0faf31
J.Cole <ccjohn015@gmail.com>
2022-03-15 09:02:40 -0500

    switched jpg to png

---
+++ backgrounds/imageArray.js
@@ -0,0 +1,10 @@
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = './backgrounds/images/redGrave.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = './backgrounds/images/cityScape.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = './backgrounds/images/sunScene.jpg';
