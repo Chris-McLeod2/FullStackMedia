376815547020a56b9d717c69ab6561b23f85fbd5
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-03-15 12:03:10 -0500

    minor changes

--- public/javascript/profiles.js
+++ public/javascript/profiles.js
@@ -1,18 +1,23 @@
 
-fetch(`/api/profile/`).then(res=>res.json()).then(data=>{
- jsonData = data;
+fetch(`/api/profile/`).then(res => res.json()).then(data => {
+    jsonData = data;
 
 
- for (let i = 0; i < data.length; i++) {
-     let user = data[i];
+    for (let i = 0; i < data.length; i++) {
+        let user = data[i];
 
-     document.querySelector('.profile-cards').innerHTML +=
-         `<div class="profile-card">
-             <h1>${user.name}</h1>
+        document.querySelector('.profile-cards').innerHTML +=
+            `<div class="profile-card">
+            <h1>Username:</h1>
+             <h2>${user.name}</h2>
+            <h1>Title:</h1>
              <h2>${user.title}</h2>
+            <h1>Bio:</h1>
              <h2>${user.bio}</h2>
+            <h1>Main Languages:</h1>
              <h2>${user.main_languages}</h2>
+            <h1>Languages to learn:</h1>
              <h2>${user.to_learn}</h2>
          </div>`
- }
+    }
 });
--- public/javascript/show-profile.js
+++ public/javascript/show-profile.js
@@ -5,10 +5,15 @@ fetch(`/api/profile/${id}`).then(res=>res.json()).then(data=>{
 
 document.querySelector('.profile-card').innerHTML +=
     `<div>
-        <h1>${data.name}</h1>
+        <h1>Username:</h1>
+        <h2>${data.name}</h2>
+        <h1>Title:</h1>
         <h2>${data.title}</h2>
+        <h1>Bio:</h1>
         <h2>${data.bio}</h2>
+        <h1>Main Languages:</h1>
         <h2>${data.main_languages}</h2>
+        <h1>Languages to learn:</h1>
         <h2>${data.to_learn}</h2>
     </div>`
 });
